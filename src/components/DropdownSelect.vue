4a7d93544c62b01abf49a6ca49233fac2919eb99
Brian Cherinka <havok2063@hotmail.com>
2024-01-19 11:50:04 -0500

    adding dropdown select for carton fields

---
+++ src/components/DropdownSelect.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-autocomplete
+      :id="id"
+      v-model="internalValue"
+      :label="label"
+      :items="items"
+      density="compact"
+      hide-details="auto"
+      clearable
+    ></v-autocomplete>
+  </template>
+
+<script lang="ts" setup>
+
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:modelValue'])
+
+// v-model cannot be used on a local property, local props are not writable.
+const internalValue = computed({
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val || "")
+});
+
+// define which properties are passed in from the parent, i.e. ":xxx"
+const props = defineProps<{
+    modelValue: string,
+    label: string,
+    id: string,
+    items: Array<string>,
+}>()
+
+</script>
