3bf751659751dc29b910af9f2eb575a11899d835
havok2063 <havok2063@hotmail.com>
2025-11-11 16:05:01 -0500

    adding some console

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,4 +4,7 @@
 
 <script lang="ts" setup>
 import HelloWorld from '@/components/HelloWorld.vue'
+
+console.log('mode', import.meta.env.MODE)
+console.log('base url', import.meta.env.VITE_BASE_URL)
 </script>
