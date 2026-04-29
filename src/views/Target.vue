66af3d593f48d0a456d8fa4ec9b3ec008083ed38
havok2063 <havok2063@hotmail.com>
2026-04-29 12:36:48 -0400

    init redo of target pipeline panels

--- src/views/Target.vue
+++ src/views/Target.vue
@@ -58,7 +58,7 @@
                                     <!-- astra source info -->
                                     <v-expansion-panel title="Astra Source Info">
                                         <v-expansion-panel-text>
-                                            <v-data-table-virtual :items="convert_object(pipelines.astra)" density="compact"></v-data-table-virtual>
+                                            <v-data-table-virtual :items="convert_object(pipelines.astra.source)" density="compact"></v-data-table-virtual>
                                         </v-expansion-panel-text>
                                     </v-expansion-panel>
 
@@ -78,17 +78,92 @@
                                     <!-- boss drp info -->
                                     <v-expansion-panel title="Boss DRP Pipeline Info">
                                         <v-expansion-panel-text>
-                                            <v-data-table-virtual :items="convert_to_table(pipelines.boss, 'boss_drp')" density="compact">
-                                                <template v-slot:item.display_name="{ item }">
+                                            <v-data-table-virtual :items="pipelines.boss" :headers="bosshead" density="compact">
+                                                <!-- <template v-slot:item.display_name="{ item }">
                                                     <p v-tippy="item.description">{{ item.display_name }}</p>
+                                                </template> -->
+                                                <!-- pipe info menu item -->
+                                                <template v-slot:item.pipeinfo="{ item }">
+                                                    <v-btn color="success" density="compact" rounded="0" variant="tonal">Show</v-btn>
+                                                </template>
+                                                <template v-slot:item.specprimary="{ item }">
+                                                    <v-icon v-if="item.specprimary">mdi-check</v-icon>
+                                                </template>
+                                                <template v-slot:item.stem="{ item }">
+                                                    <a v-if="item.stem" :href="'https://data.sdss5.org/sas/' + item.location" target="_blank">{{ item.stem }}</a>
+                                                    <span v-else>No product available</span>
                                                 </template>
                                             </v-data-table-virtual>
                                         </v-expansion-panel-text>
                                     </v-expansion-panel>
+
                                     <!-- apogee drp info -->
                                     <v-expansion-panel title="Apogee DRP Pipeline Info">
                                         <v-expansion-panel-text>
-                                            <v-data-table-virtual :items="convert_object(pipelines.apogee)" density="compact"></v-data-table-virtual>
+                                            <!--<v-data-table-virtual :items="convert_object(pipelines.apogee)" density="compact"></v-data-table-virtual>-->
+                                                <v-expansion-panels variant="accordion" multiple v-model="apopanels">
+                                                <v-expansion-panel title="Stars">
+                                                    <v-expansion-panel-text>
+                                                    <v-data-table-virtual
+                                                        :items="apogeeStars"
+                                                        :headers="apogeeStarsHead"
+                                                        item-value="pk"
+                                                        density="compact"
+                                                        :sort-by="[{ key: 'starver', order: 'asc' }]"
+                                                    >
+                                                        <!-- pipe info menu item -->
+                                                        <template v-slot:item.pipeinfo="{ item }">
+                                                            <v-btn color="success" density="compact" rounded="0" variant="tonal">Show</v-btn>
+                                                        </template>
+                                                        <template #item.stem="{ item }">
+                                                        <a v-if="item.stem" :href="'https://data.sdss5.org/sas/' + item.location" target="_blank">
+                                                            {{ item.stem }}
+                                                        </a>
+                                                        <span v-else>No product available</span>
+                                                        </template>
+                                                    </v-data-table-virtual>
+                                                    </v-expansion-panel-text>
+                                                </v-expansion-panel>
+
+                                                <v-expansion-panel title="Visits">
+                                                    <v-expansion-panel-text>
+                                                    <v-data-table-virtual
+                                                        :items="apogeeVisits"
+                                                        :headers="apogeeVisitsHead"
+                                                        item-value="pk"
+                                                        density="compact"
+                                                        :sort-by="[{ key: 'mjd', order: 'asc' }]"
+                                                    >
+                                                        <!-- pipe info menu item -->
+                                                        <template v-slot:item.pipeinfo="{ item }">
+                                                            <v-btn color="success" density="compact" rounded="0" variant="tonal">Show</v-btn>
+                                                        </template>
+                                                        <template #item.stem="{ item }">
+                                                        <a v-if="item.stem" :href="'https://data.sdss5.org/sas/' + item.location" target="_blank">
+                                                            {{ item.stem }}
+                                                        </a>
+                                                        <span v-else>No product available</span>
+                                                        </template>
+                                                    </v-data-table-virtual>
+                                                    </v-expansion-panel-text>
+                                                </v-expansion-panel>
+                                                </v-expansion-panels>
+
+
+                                        </v-expansion-panel-text>
+                                    </v-expansion-panel>
+
+                                   <!-- astra products info -->
+                                    <v-expansion-panel title="Astra Products">
+                                        <v-expansion-panel-text>
+                                            <v-data-table-virtual :headers="astraHead" :items="pipelines.astra.products" density="compact">
+                                                <template #item.stem="{ item }">
+                                                    <a v-if="item.stem" :href="'https://data.sdss5.org/sas/' + item.location" target="_blank">
+                                                        {{ item.stem }}
+                                                    </a>
+                                                    <span v-else>No product available</span>
+                                                </template>
+                                            </v-data-table-virtual>
                                         </v-expansion-panel-text>
                                     </v-expansion-panel>
 
@@ -196,6 +271,7 @@ let legacydata = ref([])
 let cartSort = [{ key: 'run_on', order: 'desc' }]
 let metapanels = ref([0])
 let pipepanels = ref(null)
+let apopanels = ref([0])
 let files = ref([])
 let has_files = ref(false)
 
@@ -225,6 +301,52 @@ let headcart = [
     {key: 'category', title: 'Category'},
     {key: 'run_on', title: 'Date Run On'},
 ]
+
+let bosshead = [
+    {key: 'product', title: 'Product'},
+    {key: 'mjd', title: 'MJD'},
+    {key: 'coadd', title: 'Coadd'},
+    {key: 'specprimary', title: 'Specprimary'},
+    {key: 'stem', title: 'File'},
+    {key: 'pipeinfo', title: 'Pipeline Parameters'},
+]
+
+const apogeeStarsHead = [
+  { key: 'product', title: 'Product' },
+  { key: 'starver', title: 'StarVer' },
+  { key: 'nvisits', title: 'NVisits' },
+  { key: 'stem', title: 'File' },
+  {key: 'pipeinfo', title: 'Pipeline Parameters'},
+]
+
+const apogeeVisitsHead = [
+  { key: 'product', title: 'Product' },
+  { key: 'mjd', title: 'MJD' },
+  { key: 'field', title: 'Field' },
+  { key: 'stem', title: 'File' },
+{key: 'pipeinfo', title: 'Pipeline Parameters'},
+]
+
+const astraHead = [
+  { key: 'product', title: 'Product' },
+  { key: 'stem', title: 'File' },
+]
+
+const apogeeStars = computed(() => {
+  const stars = pipelines.value?.apogee?.stars ?? []
+  return stars.map((row: any) => ({
+    ...row,
+    product: row.product || 'apStar',
+  }))
+})
+
+const apogeeVisits = computed(() => {
+  const visits = pipelines.value?.apogee?.visits ?? []
+  return visits.map((row: any) => ({
+    ...row,
+    product: row.product || 'apVisit',
+  }))
+})
 
 let headmeta = [
     {key: 'display_name', title: 'Display Name'},
